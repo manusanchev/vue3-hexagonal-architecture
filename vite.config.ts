e524cf6b1456e6baeeae047e77abdc63a8822d06
Manuel Sánchez Velázquez <manuel.sanchez.velazquez98@gmail.com>
2025-02-15 09:43:29 +0100

    remove library

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
-import dynamicImport from 'vite-plugin-dynamic-import'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    tailwindcss(),
-    dynamicImport(),
+    tailwindcss()
   ],
   server: {
     port: 3000,
