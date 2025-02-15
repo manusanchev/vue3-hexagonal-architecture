b7196a7d560044109d7a17e4f55ad6c2d4618bd2
Manuel Sánchez Velázquez <manuel.sanchez.velazquez98@gmail.com>
2025-02-15 10:18:48 +0100

    remove use case from container

--- src/config/container/container.ts
+++ src/config/container/container.ts
@@ -4,17 +4,12 @@ const repositoriesContainer = {
   [Types.EMPLOYEE_REPOSITORY]: () => import('@/core/employee/infrastructure/apiEmployeeRepository.ts'),
 }
 
-const applicationContainer = {
-  [Types.GET_ALL_EMPLOYEES]: () => import('@/core/employee/application/getAllEmployees.ts'),
-}
-
 const infrastructureContainer = {
   [Types.HTTP_CLIENT]: () => import('@/core/shared/infrastructure/fetchClient.ts'),
 }
 
 const container: Record<string, () => Promise<any>> = {
   ...repositoriesContainer,
-  ...applicationContainer,
   ...infrastructureContainer,
 }
 
