28ea30098c671610e160454d36ff342b3864c84e
Manuel Sánchez Velázquez <manuel.sanchez.velazquez98@gmail.com>
2025-05-27 20:29:41 +0200

    fix: Add user-invalid class to input component for improved validation styling

--- src/components/input/VInputBase.vue
+++ src/components/input/VInputBase.vue
@@ -6,6 +6,6 @@ const model = defineModel()
   <input
     v-bind="$attrs"
     v-model="model"
-    class="block w-full rounded-lg border border-neutral-300 px-3 py-2 text-sm text-neutral-800 focus:outline-none focus:ring-1 focus:ring-neutral-900"
+    class="block user-invalid:border-red-500 w-full rounded-lg border border-neutral-300 px-3 py-2 text-sm text-neutral-800 focus:outline-none focus:ring-1 focus:ring-neutral-900"
   />
 </template>
